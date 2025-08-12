e25dd45c11f4a4d43faa232c12452a801cb5383a
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-08-12 18:22:55 +0300

    done

--- lib/bool_functions.py
+++ lib/bool_functions.py
@@ -1,2 +1,23 @@
 def return_true():
-    return False
+    return True
+
+#Running Tests with Pytest
+# running all tests we use pytest
+
+# running all tests in the current directory
+# pytest lib/testing
+
+# running all tests in a file
+# pytest lib/testing/test_bool_functions.py
+
+#running one test in a file
+# pytest lib/testing/test_bool_functions.py::test_return_true
+
+#customizing the output
+# -x excutes test until one fails then stops
+# -pdb opens the debugger on failure
+# -s tells pytest to show the full output for failed tests i.e # print statements
+# -v verbose output
+# -q quiet output
+# -q shorten the output
+# -h shows help
